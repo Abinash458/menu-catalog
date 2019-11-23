0cf7668746dab6cff099368223c5f198146984b3
Abinash <amohapatra458@gmail.com>
2019-11-23 21:08:14 +0530

    Add the AboutComponent

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -35,8 +35,8 @@ function Contact(props) {
                 </div>
                 <div className="col-12 col-sm-11 offset-sm-1">
                     <div className="btn-group" role="group">
-                        <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                        <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
+                        <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a> &nbsp;
+                        <a href="/" role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a> &nbsp;
                         <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
                     </div>
                 </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import Dishdetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -48,6 +49,7 @@ class Main extends Component {
         <Header />
           <Switch>
             <Route path='/home' component={HomePage} />
+            <Route path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path='/contactus' component={Contact} />
